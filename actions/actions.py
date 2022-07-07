fb4a79e37453f2eacdfc9f4ef60dea581b228e6c
João Vitor <joaovitor.felipesantos@gmail.com>
2022-07-07 19:02:31 -0300

    WIP: Create reminder categories: alarm, timer and reminder

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,7 @@
 import yaml
 import os
 from dateutil import parser
+from . import utils
 
 from typing import Any, Text, Dict, List, Optional
 from rasa_sdk import Action, Tracker, FormValidationAction
@@ -8,11 +9,9 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import (SlotSet, EventType)
 from rasa_sdk.types import DomainDict
 
-from datetime import datetime
-
 ################################## GENERAL ACTIONS ##################################
 class ActionGreetUser(Action):
-    """ Greets user by time of day, or just a normal greeting isn't the first use of the time period """
+    """ Greets user by time of day, or just a normal greeting isn't the first use of the time period"""
 
     def name(self) -> Text:
         return "action_greet_user"
@@ -21,24 +20,23 @@ class ActionGreetUser(Action):
         last_time_period = tracker.get_slot("time_period")
         # name_entity = next(tracker.get_latest_entity_values("name"), None)
 
-        time_utils = UtilsTime()
-        actual_period = time_utils.get_actual_period()
+        actual_period = utils.get_actual_period()
 
         if(last_time_period and last_time_period == actual_period):
             # if(name_entity):
             #     dispatcher.utter_message(template="utter_name_greeting", name=name_entity)
             # else:
-            dispatcher.utter_message(template="utter_general_greeting")
+            dispatcher.utter_message(reponse="utter_general_greeting")
             return[]
         else:
             if(actual_period == "morning"):
-                dispatcher.utter_message(template="utter_good_morning")
+                dispatcher.utter_message(reponse="utter_good_morning")
             elif(actual_period == "afternoon"):
-                dispatcher.utter_message(template="utter_good_afternoon")
+                dispatcher.utter_message(reponse="utter_good_afternoon")
             elif(actual_period == "evening"):
-                dispatcher.utter_message(template="utter_good_evening")
+                dispatcher.utter_message(reponse="utter_good_evening")
             elif(actual_period == "night"):
-                dispatcher.utter_message(template="utter_good_night")
+                dispatcher.utter_message(reponse="utter_good_night")
 
             return [SlotSet("time_period", actual_period)]
 
@@ -54,7 +52,7 @@ class ActionStartResource(Action):
         try:
             dir_path = os.path.dirname(os.path.realpath(__file__))
             path = os.path.join(dir_path, "resources.yml")
-            resource_name = tracker.get_slot("resource")[0].lower().replace(" ", "_")
+            resource_name = tracker.get_slot("resource").lower().replace(" ", "_")
 
             with open(path, 'r') as file:
                 resource_list = yaml.load(file, Loader=yaml.FullLoader)
@@ -72,12 +70,12 @@ class ActionStartResource(Action):
                     if found_resource:
                         break           
         except:
-            dispatcher.utter_message(template="utter_resource_notfound")
+            dispatcher.utter_message(reponse="utter_resource_notfound")
             return
         if found_resource:
-            dispatcher.utter_message(template="utter_opening_resource", resource=resource_name)
+            dispatcher.utter_message(reponse="utter_opening_resource", resource=resource_name)
             return [SlotSet("last_opened_resource", resource_name)]
-        dispatcher.utter_message(template="utter_resource_notfound")
+        dispatcher.utter_message(reponse="utter_resource_notfound")
         return
 
 class ValidateReminderForm(FormValidationAction):
@@ -117,7 +115,46 @@ class ValidateReminderForm(FormValidationAction):
 
             return {"reminder_time": reminder_time}
         return {"reminder_time": None}
+
+class ValidateAlarmForm(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_alarm_form"
+
+    @staticmethod
+    def validate_reminder_time(
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        reminder_time = {}
+        print("entrou aqui na validação")
+
+        # Gets event time sent on the last message
+        if len(tracker.latest_message['entities']):
+            for entity in tracker.latest_message['entities']:
+                if(entity['entity'] == 'time'):
+                    time = entity
         
+        print("setou time", time)
+
+        if not time == None:
+            grain = time['additional_info']['grain']
+            date_time_obj = parser.parse(time['value'])
+
+            if(grain == 'hour'):
+                reminder_date = date_time_obj.strftime("%d-%m-%Y")
+                reminder_time = date_time_obj.strftime("%H:%M:%S")
+            elif(grain == 'day'):
+                reminder_date = date_time_obj.strftime("%d-%m-%Y")
+            
+            reminder_time = {'date': reminder_date, 'time': reminder_time, 'grain': grain}
+            print("setou time", reminder_time)
+
+            return {"reminder_time": reminder_time}
+        return {"reminder_time": None}
+        
+
 class ActionSetReminder(Action):
     """ Set an basic reminder based on the user request"""
     def name(self) -> Text:
@@ -126,14 +163,39 @@ class ActionSetReminder(Action):
     def run(self, dispatcher, tracker, domain) -> List[EventType]: 
         reminder_time = tracker.get_slot("reminder_time")
         reminder_name = tracker.get_slot("reminder_name")
+        reminder_type = tracker.get_slot("reminder_type")
 
         print("time", reminder_time, reminder_name)
 
         reminder_info = {
-            'date': reminder_time['date'], 'time': reminder_time['time'], 'name': reminder_name
+            'date': reminder_time['date'], 'time': reminder_time['time'], 'name': reminder_name, 'type': reminder_type 
         }
         return [SlotSet("reminder_info", reminder_info), SlotSet("reminder_name", None), SlotSet("reminder_time", None)]
 
+
+class ActionSetReminderType(Action):
+    """ Set reminder type as reminder"""
+    def name(self) -> Text:
+        return "action_set_reminder_type"
+
+    def run(self, dispatcher, tracker, domain) -> List[EventType]: 
+        return [SlotSet("reminder_type", 'reminder')]
+
+class ActionSetAlarmType(Action):
+    """ Set reminder type as alarm """
+    def name(self) -> Text:
+        return "action_set_alarm_type"
+
+    def run(self, dispatcher, tracker, domain) -> List[EventType]: 
+        return [SlotSet("reminder_type", 'alarm')]
+
+class ActionSetTimerType(Action):
+    """ Set reminder type as timer"""
+    def name(self) -> Text:
+        return "action_set_timer_type"
+
+    def run(self, dispatcher, tracker, domain) -> List[EventType]: 
+        return [SlotSet("reminder_type", 'timer')]
 ################################## SMART HOME ACTIONS ##################################
 class ActionExecuteSmartHomeAction(Action):
     """Execute a generic action related to home automation"""
@@ -143,40 +205,3 @@ class ActionExecuteSmartHomeAction(Action):
     def run(self, dispatcher, tracker, domain) -> List[EventType]: 
         ## Get the last intent, and return the action and room based on it.
         return
-
-################################## UTILS FUNCTIONS ##################################
-class UtilsTime():
-    def get_part_of_day(self, hour):
-        return (
-            "morning" if 5 <= hour <= 11
-            else
-            "afternoon" if 12 <= hour <= 17
-            else
-            "evening" if 18 <= hour <= 23
-            else
-            "night"
-        )
-
-    def get_actual_period(self):
-        h = datetime.now().hour
-        return self.get_part_of_day(h)
-
-class UtilsRasa():
-    def get_last_utter_action(self, tracker):
-        print("tracker", tracker)
-        ##goes back through the list of events and finds
-        ##the last utter_action
-        for event in reversed(tracker.events):
-            try:
-                #print("current action name is", event.get('name'))
-                if event.get('name') not in [ 'action_listen', None ] :
-                    last_utter_action = event.get('name')
-                    print('found action', last_utter_action)
-                    return last_utter_action
-                else :
-                    #print(event.get('name'))
-                    pass
-            except:
-                pass
-                #print(event.get('text'))
-        return 'error! no last action found'
---
+++ actions/utils.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+
+def get_part_of_day(hour):
+    return (
+        "morning" if 5 <= hour <= 11
+        else
+        "afternoon" if 12 <= hour <= 17
+        else
+        "evening" if 18 <= hour <= 23
+        else
+        "night"
+    )
+
+def get_actual_period():
+    h = datetime.now().hour
+    return get_part_of_day(h)
+
+def get_last_utter_action(tracker):
+    print("tracker", tracker)
+    ##goes back through the list of events and finds
+    ##the last utter_action
+    for event in reversed(tracker.events):
+        try:
+            #print("current action name is", event.get('name'))
+            if event.get('name') not in [ 'action_listen', None ] :
+                last_utter_action = event.get('name')
+                print('found action', last_utter_action)
+                return last_utter_action
+            else :
+                #print(event.get('name'))
+                pass
+        except:
+            pass
+            #print(event.get('text'))
+    return 'error! no last action found'
